8663db7fdf5c7432125cf3eb4225ca9a65b0e747
Devansh Arora <aroradevansh26@gmail.com>
2025-08-24 16:17:16 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 // client/src/App.jsx
 
 import React from 'react';
-// Corrected the typo in the import statement below
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Page & Component Imports
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
@@ -13,21 +11,17 @@ import ProtectedRoute from './components/ProtectedRoute.jsx';
 import Header from './components/Header.jsx';
 import LinkAnalyticsPage from './pages/LinkAnalyticsPage.jsx';
 
-// CSS Imports
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="main-content-area">
         <Routes>
-          {/* Public Routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-
-          {/* Protected Route */}
           <Route
             path="/dashboard"
             element={
@@ -36,7 +30,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-            <Route
+          <Route
             path="/dashboard/links/:id"
             element={
               <ProtectedRoute>
@@ -46,7 +40,7 @@ function App() {
           />
         </Routes>
       </main>
-    </>
+    </Router>
   );
 }
 
